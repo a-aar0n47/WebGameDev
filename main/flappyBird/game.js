35735f6c473ac072a05d5e647cc75790f04b5df4
Aaron <aaronmusisi4@gmail.com>
2023-08-13 19:49:13 +0300

    Final commits for game: Flappy Bird

--- main/flappyBird/game.js
+++ main/flappyBird/game.js
@@ -1,3 +1,4 @@
+
 //canvas
 let canvas;
 let canvasWidth = 360;
@@ -5,46 +6,52 @@ let canvasHeight = 640;
 let context;
 
 //bird
-const birdWidth = 34; //width/height ratio = 408x228 = ratio of 17:12
-const birdHeight = 24;
-//Positions of bird
-const birdX = canvasWidth / 8
-const birdY = canvasHeight / 2;
-//bird image
+let birdWidth = 34; //width/height ratio = 408/228 = 17/12
+let birdHeight = 24;
+let birdX = canvasWidth/8;
+let birdY = canvasHeight/2;
 let birdImg;
 
-const bird = {
-    x: birdX,
-    y: birdY,
-    width: birdWidth,
-    height: birdHeight
+let bird = {
+    x : birdX,
+    y : birdY,
+    width : birdWidth,
+    height : birdHeight
 }
 
-
 //pipes
-let pipeArray = [];//empty array for pipe storage
-const pipeWidth = 64;//width/height ratio = 384/3072 = 1/8
-const pipeHeight = 512;
+let pipeArray = [];
+let pipeWidth = 64; //width/height ratio = 384/3072 = 1/8
+let pipeHeight = 512;
 let pipeX = canvasWidth;
 let pipeY = 0;
 
 let topPipeImg;
 let bottomPipeImg;
-window.onload = function() {//making sure page has loaded before executing any code
+
+//physics
+let velocityX = -2; //pipes moving left speed
+let velocityY = 0; //bird jump speed
+let gravity = 0.4;
+
+let gameOver = false;
+let score = 0;
+
+window.onload = function() {
     canvas = document.getElementById("canvas");
     canvas.height = canvasHeight;
-    canvas.width = canvasWidth
-    context = canvas.getContext("2d");
+    canvas.width = canvasWidth;
+    context = canvas.getContext("2d"); //used for drawing on the canvas
 
-    //drawing flappy bird
-    //context.fillStyle = "green";
-    //context.fillRect(bird.x, bird.y, bird.width, bird.height);
+    //draw flappy bird
+    // context.fillStyle = "green";
+    // context.fillRect(bird.x, bird.y, bird.width, bird.height);
 
-    //loading images
+    //load images
     birdImg = new Image();
     birdImg.src = "/main/flappyBird/images/flappybird.png";
     birdImg.onload = function() {
-        context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height)
+        context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
     }
 
     topPipeImg = new Image();
@@ -54,37 +61,114 @@ window.onload = function() {//making sure page has loaded before executing any c
     bottomPipeImg.src = "/main/flappyBird/images/bottompipe.png";
 
     requestAnimationFrame(update);
-    setInterval(placePipes, 1500);//every 1.5 seconds
-
+    setInterval(placePipes, 1500); //every 1.5 seconds
+    document.addEventListener("keydown", moveBird);
 }
 
-let update = () => {
-    //main function for redrawing canvas
+function update() {
     requestAnimationFrame(update);
-    //clearing previous frame after every update
+    if (gameOver) {
+        return;
+    }
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     //bird
+    velocityY += gravity;
+    // bird.y += velocityY;
+    bird.y = Math.max(bird.y + velocityY, 0); //apply gravity to current bird.y, limit the bird.y to top of the canvas
     context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
 
-    //pipes
+    if (bird.y > canvas.height) {
+        gameOver = true;
+    }
 
+    //pipes
     for (let i = 0; i < pipeArray.length; i++) {
         let pipe = pipeArray[i];
+        pipe.x += velocityX;
         context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
-        console.log("Test!")
+
+        if (!pipe.passed && bird.x > pipe.x + pipe.width) {
+            score += 0.5; //0.5 because there are 2 pipes! so 0.5*2 = 1, 1 for each set of pipes
+            pipe.passed = true;
+        }
+
+        if (detectCollision(bird, pipe)) {
+            gameOver = true;
+        }
+    }
+
+    //clear pipes
+    while (pipeArray.length > 0 && pipeArray[0].x < -pipeWidth) {
+        pipeArray.shift(); //removes first element from the array
+    }
+
+    //score
+    context.fillStyle = "white";
+    context.font="45px Poppins";
+    context.fillText(score, 5, 45);
+
+    if (gameOver) {
+        context.fillStyle = "red";
+        context.fillText("GAME OVER", 9, 90);
     }
 }
-let placePipes = () => {
 
-    let topPipe = {
-        img: topPipeImg,
-        x: pipeX,
-        y: pipeY,
-        width: pipeWidth,
-        height: pipeWidth,
-        passed: false //if flappy bird has passed pipe
+function placePipes() {
+    if (gameOver) {
+        return;
     }
 
+    //(0-1) * pipeHeight/2.
+    // 0 -> -128 (pipeHeight/4)
+    // 1 -> -128 - 256 (pipeHeight/4 - pipeHeight/2) = -3/4 pipeHeight
+    let randomPipeY = pipeY - pipeHeight/4 - Math.random()*(pipeHeight/2);
+    let openingSpace = canvas.height/4;
+
+    let topPipe = {
+        img : topPipeImg,
+        x : pipeX,
+        y : randomPipeY,
+        width : pipeWidth,
+        height : pipeHeight,
+        passed : false
+    }
     pipeArray.push(topPipe);
+
+    let bottomPipe = {
+        img : bottomPipeImg,
+        x : pipeX,
+        y : randomPipeY + pipeHeight + openingSpace,
+        width : pipeWidth,
+        height : pipeHeight,
+        passed : false
+    }
+    pipeArray.push(bottomPipe);
+}
+
+function moveBird(e) {
+    if (e.code == "Space" || e.code == "ArrowUp" || e.code == "KeyX") {
+        //jump
+        velocityY = -6;
+
+        //reset game
+        if (gameOver) {
+            bird.y = birdY;
+            pipeArray = [];
+            score = 0;
+            gameOver = false;
+        }
+    }
+}
+
+function detectCollision(a, b) {
+    return a.x < b.x + b.width &&   //a's top left corner doesn't reach b's top right corner
+           a.x + a.width > b.x &&   //a's top right corner passes b's top left corner
+           a.y < b.y + b.height &&  //a's top left corner doesn't reach b's bottom left corner
+           a.y + a.height > b.y;    //a's bottom left corner passes b's top left corner
+}
+const saveScore = () => {
+    let scoreCount = document.getElementById("scoreCount")
+    let scoreCountValue = scoreCount.value
+    scoreCount.innerHTML = `Previous score: ${score}`
 }
